aa173d3e37192eeadcc2c05c6f0d70c84881cbb0
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2022-10-04 01:54:22 +0700

    fix: Temporarily limit max articles for deep cleaning

--- deepclean.php
+++ deepclean.php
@@ -2,14 +2,16 @@
 require_once('./dbconnection.php');
 
 print('Starting deep cleaning process...' . PHP_EOL);
-$total_articles = db::table('articles')->count();
+// $total_articles = db::table('articles')->count();
+$total_articles = 2000;
 print("There are $total_articles articles available" . PHP_EOL);
 $index = 0;
 $removed = 0;
 
 while ($index < $total_articles) {
     $TAKE = 10;
-    $articles = db::table('articles')->select(['id'])->skip($index)->take(15)->get();
+    $articles = db::table('articles')->select(['id'])->skip($index)->take(15)->orderBy('timestamp', 'desc')->get();
+    $currently_removed = 0;
     foreach ($articles as $article) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $article->id);
@@ -22,12 +24,13 @@ while ($index < $total_articles) {
 
         if ($is404) {
             db::table('articles')->where('id', $article->id)->delete();
-            $removed++;
+            $currently_removed++;
             print("X | Removed $article->id" . PHP_EOL);
         }
 
     }
-    $index += 10;
+    $index += 10 - $currently_removed;
+    $removed += $currently_removed;
     sleep(10);
 }
 print("Deep cleaning complete, with $removed posts removed" . PHP_EOL);
