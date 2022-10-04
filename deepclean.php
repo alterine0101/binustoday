1a134d2333341710009203ac1ac1ce21b4eee088
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2022-10-04 10:46:58 +0700

    feat: Add skip and take parameters

--- deepclean.php
+++ deepclean.php
@@ -2,15 +2,30 @@
 require_once('./dbconnection.php');
 
 print('Starting deep cleaning process...' . PHP_EOL);
-// $total_articles = db::table('articles')->count();
-$total_articles = 2000;
+
+$skip = getenv('DEEPCLEAN_SKIP');
+$skip = $skip == false ? 0 : ((int)$skip);
+
+$total_articles = getenv('DEEPCLEAN_TAKE');
+$total_articles = ($total_articles == false ||  (int)$total_articles <= 0) ? db::table('articles')->count() : (int)$total_articles;
+
+$filter = getenv('DEEPCLEAN_FILTER');
+if ($filter != false) {
+  $filter = stripslashes(mysql_real_escape_string($filter));
+}
+
+print("Skipping $skip articles" . PHP_EOL);
 print("There are $total_articles articles available" . PHP_EOL);
 $index = 0;
 $removed = 0;
 
 while ($index < $total_articles) {
     $TAKE = 10;
-    $articles = db::table('articles')->select(['id'])->skip($index)->take(15)->orderBy('timestamp', 'desc')->get();
+    $articles = db::table('articles')->select(['id'])->skip($skip + $index);
+    if ($filter != false) {
+        $articles->whereRaw("id LIKE $filter");
+    }
+    $articles = $articles->take(15)->orderBy('timestamp', 'desc')->get();
     $currently_removed = 0;
     foreach ($articles as $article) {
         $test = get_headers($article->id);
